8e947ad43bc4871b0d6fa420f6ad44aea68803a6
LEGION <adityagilangpangestu7@gmail.com>
2022-12-31 15:44:57 +0700

    commit

---
+++ app/src/main/java/com/example/toko_lang_13492/HomeActivity.java
@@ -0,0 +1,72 @@
+package com.example.toko_lang_13492;
+
+import androidx.annotation.NonNull;
+import androidx.appcompat.app.AppCompatActivity;
+import androidx.fragment.app.Fragment;
+import androidx.fragment.app.FragmentTransaction;
+
+import android.os.Bundle;
+import android.view.MenuItem;
+
+import com.example.toko_lang_13492.fragment.AddFragment;
+import com.example.toko_lang_13492.fragment.CartFragment;
+import com.example.toko_lang_13492.fragment.CatFragment;
+import com.example.toko_lang_13492.fragment.HomeFragment;
+import com.google.android.material.bottomnavigation.BottomNavigationView;
+
+public class HomeActivity extends AppCompatActivity implements BottomNavigationView.OnNavigationItemSelectedListener {
+
+    BottomNavigationView bottomNavigationView;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_home);
+        init();
+    }
+    void init(){
+        getSupportActionBar().hide();
+        bottomNavigationView = findViewById(R.id.bottomNavigationView);
+
+        bottomNavigationView.setOnNavigationItemSelectedListener(this);
+        bottomNavigationView.setSelectedItemId(R.id.home);
+    }
+    HomeFragment homeFragment = new HomeFragment();
+    CartFragment cartFragment = new CartFragment();
+    AddFragment addFragment = new AddFragment();
+    Fragment fragment = null;
+    @Override
+    public boolean onNavigationItemSelected(@NonNull MenuItem item) {
+
+        switch (item.getItemId()) {
+            case R.id.home:
+                fragment = homeFragment;
+                getSupportFragmentManager().beginTransaction().replace(R.id.flFragment, fragment).commit();
+                break;
+            case R.id.cart:
+                fragment = cartFragment;
+                getSupportFragmentManager().beginTransaction().replace(R.id.flFragment, fragment).detach(fragment).commitNow();
+                getSupportFragmentManager().beginTransaction().replace(R.id.flFragment, fragment).attach(fragment).commitNow();
+                break;
+            case R.id.add:
+                fragment = addFragment;
+                getSupportFragmentManager().beginTransaction().replace(R.id.flFragment, fragment).commit();
+                break;
+        }
+        /*if (fragment != null) {
+            loadFragment(fragment);
+        }else{
+            loadFragment(addFragment);
+        }*/
+        return true;
+    }
+    void loadFragment(Fragment fragment) {
+        //to attach fragment
+        if (fragment == cartFragment){
+            getSupportFragmentManager().beginTransaction().replace(R.id.flFragment, fragment).detach(fragment).attach(fragment).commit();
+        }else{
+            getSupportFragmentManager().beginTransaction().replace(R.id.flFragment, fragment).commit();
+        }
+
+    }
+}
---
+++ app/src/main/java/com/example/toko_lang_13492/MainActivity.java
@@ -0,0 +1,39 @@
+package com.example.toko_lang_13492;
+
+import androidx.annotation.NonNull;
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.content.Intent;
+import android.os.Bundle;
+import android.text.Layout;
+import android.view.View;
+import android.widget.Button;
+import android.widget.EditText;
+import android.widget.RelativeLayout;
+import android.widget.Spinner;
+import android.widget.Toast;
+
+import com.google.firebase.database.DataSnapshot;
+import com.google.firebase.database.DatabaseError;
+import com.google.firebase.database.DatabaseReference;
+import com.google.firebase.database.FirebaseDatabase;
+import com.google.firebase.database.ValueEventListener;
+
+public class MainActivity extends AppCompatActivity {
+
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+        getSupportActionBar().hide();
+        Button btnKon = findViewById(R.id.btn_continue);
+        btnKon.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                Intent intent = new Intent(MainActivity.this, HomeActivity.class);
+                startActivity(intent);
+            }
+        });
+    }
+}
